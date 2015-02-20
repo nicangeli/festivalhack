77e4df93bc5c59619f47e9a2d531f3159ac1c224
Michael Patterson-Muir <Michael.Patterson-Muir@bbc.co.uk>
2015-02-20 19:16:19 +0000

    array splice

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,16 +12,35 @@ app.controller('MainController', function($scope, $http, $sce, $interval) {
 	$scope.vines = [];
 	$scope.rawVines = [];
 	$scope.fetchFromVine = function() {
-		console.log('pull');
+		//console.log('pull');
 		$http.get(vine_url)
 			.success(function(data) {
-				console.log(data);
+				//console.log(data);
 				data.data.records.forEach(function(vine){
 					if($scope.rawVines.indexOf(vine.videoUrl) == -1){
 						$scope.vines.push($sce.trustAsResourceUrl(vine.videoUrl));
-						$scope.rawVines.push(vine.videoUrl);	
+						$scope.rawVines.push(vine.videoUrl);
 					}
-				});							
+				});
+				$scope.vines = $scope.vines.splice(0, 15);						
+			})
+			.error(function(error) {
+				console.err(error);
+			});
+
+	};
+	$scope.fetchFromVineEnd = function() {
+		//console.log('pull');
+		$http.get(vine_url)
+			.success(function(data) {
+				//console.log(data);
+				data.data.records.forEach(function(vine){
+					if($scope.rawVines.indexOf(vine.videoUrl) == -1){
+						$scope.vines.unshift($sce.trustAsResourceUrl(vine.videoUrl));
+						$scope.rawVines.push(vine.videoUrl);
+					}
+				});					
+				$scope.vines = $scope.vines.splice(0, 15);						
 			})
 			.error(function(error) {
 				console.err(error);
@@ -29,6 +48,6 @@ app.controller('MainController', function($scope, $http, $sce, $interval) {
 	};
 	$scope.fetchFromVine();
 	$interval(function(){
-		$scope.fetchFromVine();
+		$scope.fetchFromVineEnd();
 	}, 5000);
 });
