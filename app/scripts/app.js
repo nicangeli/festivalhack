4929e1802f77afa5a45f8fbad3a4cd0a409589fa
Nicholas Angeli <nicholas.angeli@bbc.co.uk>
2015-02-20 13:05:50 +0000

    Eurh

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,7 +3,13 @@
 var app = angular.module('festivalhack', []);
 
 app.controller('MainController', function($scope) {
+	var vine_url = "https://api.vineapp.com/timelines/tags/<tag>";
 	$scope.model = {
-		contents: 'Hello Michael And Nick'
-	}
+		tag: 'Default',
+		contents: vine_url
+	};
+
+	$scope.submit = function() {
+		$scope.model.contents = vine_url.replace('<tag>', $scope.model.tag);
+	};
 });
