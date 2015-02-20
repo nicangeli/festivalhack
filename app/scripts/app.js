930cb1cbc897f3d9a71e033da0775173f54ec05c
Nicholas Angeli <nicholas.angeli@bbc.co.uk>
2015-02-20 13:20:08 +0000

    Calling Vine

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,14 +2,20 @@
 
 var app = angular.module('festivalhack', []);
 
-app.controller('MainController', function($scope) {
-	var vine_url = "https://api.vineapp.com/timelines/tags/<tag>";
+app.controller('MainController', function($scope, $http) {
+	var vine_url = "https://api.vineapp.com/timelines/tags/festivalhack";
 	$scope.model = {
-		tag: 'Default',
 		contents: vine_url
 	};
 
-	$scope.submit = function() {
-		$scope.model.contents = vine_url.replace('<tag>', $scope.model.tag);
+	$scope.fetchFromVine = function() {
+		console.log('yuouo');
+		$http.get(vine_url)
+			.success(function(data) {
+				console.log(data);
+			})
+			.error(function(error) {
+				console.err(error);
+			});
 	};
 });
